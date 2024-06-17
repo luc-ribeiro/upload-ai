5c5cdb327f3511a07b01f378946b135349b4e0aa
Lucas Ribeiro <lucasribeirodesouza@live.com>
2024-06-17 14:54:49 -0300

    feat: add promp select

---
+++ web/src/components/prompt-select.tsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+import { Select, SelectTrigger, SelectValue, SelectContent, SelectItem } from "./ui/select";
+import { api } from "@/lib/axios";
+
+interface Prompt {
+  id: string
+  title: string
+  template: string
+}
+
+interface PromptSelectProps {
+  onPromptSelected: (template: string) => void
+}
+
+export function PromptSelect({ onPromptSelected }: PromptSelectProps) {
+  const [prompts, setPrompts] = useState<Prompt[] | null>(null)
+
+  useEffect(() => {
+    api.get('/prompts').then(response => {
+      setPrompts(response.data.prompts)
+    })
+  }, [])
+
+  function handlePromptSelected(promptId: string) {
+    const selectedPrompt = prompts?.find(prompt => prompt.id === promptId)
+
+    if (!selectedPrompt) {
+      return
+    }
+
+    onPromptSelected(selectedPrompt.template)
+  }
+
+  return (
+    <Select onValueChange={handlePromptSelected}>
+      <SelectTrigger>
+        <SelectValue placeholder="Select a prompt..." />
+      </SelectTrigger>
+      <SelectContent>
+        {
+          prompts?.map((prompt) => {
+            return (
+              <SelectItem key={prompt.id} value={prompt.id}>{prompt.title}</SelectItem>
+            )
+          })
+        }
+      </SelectContent>
+    </Select>
+  )
+}
